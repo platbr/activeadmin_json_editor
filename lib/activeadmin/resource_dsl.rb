850a70da37316b23b766256fba3643b2bd286e11
Wagner Caixeta <wagner.caixeta@gmail.com>
2021-09-09 12:22:32 -0300

    Fix dumb logic

--- lib/activeadmin/resource_dsl.rb
+++ lib/activeadmin/resource_dsl.rb
@@ -7,41 +7,27 @@ module ActiveAdmin
       before_save do |object, _args|
         object.class.columns_hash.select { |_key, attr| attr.type.in? [:json, :jsonb] }.keys.each do |key|
           next unless params[resource_request_name].key? key
+          next unless object.attributes[key].is_a?(String)
 
-          json_data = params[resource_request_name][key]
-          data = if json_data == 'null' || json_data.blank?
-                   {}
-                 else
-                   JSON.parse(json_data)
-          end
-
-          object.attributes = { key => data }
+          object.attributes = { key => JSON.parse(object.send(key)) }
         end
-        object.nested_attributes_options.keys.each {|nested_key|
+        object.nested_attributes_options.keys.each do |nested_key|
           nested_attributes_with_index = params[resource_request_name]["#{nested_key}_attributes"]
           next if nested_attributes_with_index.nil?
 
           nested_klass = nested_key.to_s.singularize.camelize.constantize
           nested_klass.columns_hash.select { |_key, attr| attr.type.in? [:json, :jsonb] }.keys.each do |key|
-            nested_attributes_with_index.each { |index, nested_attributes|
+            nested_attributes_with_index.each do |_index, nested_attributes|
               next unless nested_attributes.key? key
 
-              json_data = nested_attributes[key]
-              data = if json_data == 'null' || json_data.blank?
-                       {}
-                     else
-                       JSON.parse(json_data)
-              end
-              object.send(nested_key).each { |nested_object|
-                if nested_attributes_with_index[index]['id'] && (nested_attributes_with_index[index]['id'] != nested_object.id.to_s)
-                  next
-                end
+              object.send(nested_key).each do |nested_object|
+                next unless nested_object.attributes[key].is_a?(String)
 
-                nested_object.attributes = { key => data }
-              }
-            }
+                nested_object.attributes = { key => JSON.parse(nested_object.send(key)) }
+              end
+            end
           end
-        }
+        end
       end
     end
   end
