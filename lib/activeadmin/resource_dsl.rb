3538db9ce8484f3d80a5c7eaa2302ae5cf056ca1
Wagner Caixeta <wagner.caixeta@gmail.com>
2021-09-09 11:05:39 -0300

    Fix nested associations

--- lib/activeadmin/resource_dsl.rb
+++ lib/activeadmin/resource_dsl.rb
@@ -7,6 +7,7 @@ module ActiveAdmin
       before_save do |object, _args|
         object.class.columns_hash.select { |_key, attr| attr.type.in? [:json, :jsonb] }.keys.each do |key|
           next unless params[resource_request_name].key? key
+
           json_data = params[resource_request_name][key]
           data = if json_data == 'null' || json_data.blank?
                    {}
@@ -18,10 +19,12 @@ module ActiveAdmin
         object.nested_attributes_options.keys.each {|_nested_key|
           nested_attributes_with_index = params[resource_request_name]["#{_nested_key}_attributes"]
           next if nested_attributes_with_index.nil?
+
           nested_klass = _nested_key.to_s.singularize.camelize.constantize
           nested_klass.columns_hash.select { |_key, attr| attr.type.in? [:json, :jsonb] }.keys.each do |key|
             nested_attributes_with_index.each {|_index, nested_attributes|
               next unless nested_attributes.key? key
+
               json_data = nested_attributes[key]
               data = if json_data == 'null' || json_data.blank?
                        {}
@@ -29,6 +32,8 @@ module ActiveAdmin
                        JSON.parse(json_data)
               end
               object.send(_nested_key).each {|nested_object|
+                next if nested_attributes_with_index[_index]["id"] != nested_object.id.to_s
+
                 nested_object.attributes = { key => data }
               }
             }
